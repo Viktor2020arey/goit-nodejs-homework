6b942326d3c34f5a198b30fe451a1a203814abd0
Holovatiuk Victor <Stethem696@gmail.com>
2021-08-26 01:25:20 +0300

    Закончил ДЗ1

---
+++ index.js
@@ -0,0 +1,56 @@
+const {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+} = require("./contacts");
+
+const { Command } = require("commander");
+const program = new Command();
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+(async () => {
+  const argv = program.opts();
+
+  const invokeAction = async ({ action, id, name, email, phone }) => {
+    switch (action) {
+      case "list":
+        const products = await listContacts();
+        console.table(products);
+
+        break;
+
+      case "get":
+        const getContact = await getContactById(id);
+        console.table(getContact);
+
+        break;
+
+      case "add":
+        await addContact(name, email, phone);
+        const newContacts = await listContacts();
+        console.table(newContacts);
+
+        break;
+
+      case "remove":
+        await removeContact(id);
+        const newContactsAftRemove = await listContacts();
+        console.table(newContactsAftRemove);
+
+        break;
+
+      default:
+        console.warn("\x1B[31m Unknown action type!");
+    }
+  };
+
+  await invokeAction(argv);
+})();
